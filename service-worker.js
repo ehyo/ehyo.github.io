bc6aea60fd5a004dbc0759a61630124c094dc40c
ehyo <hyoe@yahoo.com>
2020-08-04 21:01:23 -0700

    improve install experience too much work

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 var APP_PREFIX = 'cannabis_calculator'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = '_version_10'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = '_version_11'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [
     // '/https://ehyo.github.io/',
