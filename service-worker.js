d3f7703be3a48ef9f648a3a36f5e832f3ba8cd59
ehyo <hyoe@yahoo.com>
2020-08-04 20:25:27 -0700

    versioning

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,9 @@
 var APP_PREFIX = 'cannabis_calculator'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = '_version_08'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = '_version_09'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [
     // '/https://ehyo.github.io/',
+    '/',
     '/index.html',
     '/styles-light.css',
     '/styles-dark.css',
